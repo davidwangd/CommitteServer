4dfe3acc2055059ecd37c384edd875fb179aaf0b
TIANWEN <tianwen532@163.com>
2019-04-30 04:13:17 -0700

    change database to mysql

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CommitteServer',
+		'USER': 'tianwen',
+		'PASSWORD': 'YUNDAGAO',
+		'HOST': 'localhost',
+		'PORT': '',
     }
 }
 
